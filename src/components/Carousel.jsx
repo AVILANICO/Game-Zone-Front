495370dbd3e9a1c7147119fca371e270acba4053
AVILANICO <122701169+AVILANICO@users.noreply.github.com>
2023-06-11 21:09:46 -0300

    Avila 3 (#10)
    * video funcional
    
    * mygame, newgame, register, signin
    
    * fixeando
    
    * login, register, my games

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,7 @@
 import videito from '../assets/video/video-completo.mp4'
 export default function Carousel() {
   return (
-    <video className="bgimg-1"  autoPlay muted loop>
+    <video className="bgimg-1" autoPlay muted loop>
       <source src={videito} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,6 @@
 import Button from "../components/Button"
 import Carousel from "../components/Carousel"
-import { Navigate,useNavigate} from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 import { useState, } from 'react';
 import HomeCard from "./HomeCard"
@@ -9,27 +9,20 @@ import HomeCard from "./HomeCard"
 
 
 export default function Index() {
-  
-
-
-
- 
-
-
 
 
   return (
     <>
       <div className="md:pt-5 bg-[#1D1D1D]">
         <div className="bgimg-1">
-        <h2  className="not-italic font-bold text-[55px] leading-[95.19%] text-white text-shadow: 1px 8px 50px rgba(255, 255, 255, 0.25)  mt-52 ">Experience the thrill of Need for Speed</h2>
-          <p  className="font-normal text-2xl leading-[95.19%] text-white w-[55%]  mt-3">Choose your ride and dominate the streets</p>
+          <h2 className="not-italic font-bold text-[55px] leading-[95.19%] text-white text-shadow: 1px 8px 50px rgba(255, 255, 255, 0.25)  mt-52 ">Experience the thrill of Need for Speed</h2>
+          <p className="font-normal text-2xl leading-[95.19%] text-white w-[55%]  mt-3">Choose your ride and dominate the streets</p>
           <Button />
         </div>
         <header className="mx-8 mt-0">
           <Carousel />
         </header>
-        <HomeCard  />
+        <HomeCard />
       </div>
     </>
   )
