ddb8227c229d327e4db56a96f5dbba86802b03d2
Laureano Hurtado <109369745+Laureano150803@users.noreply.github.com>
2023-06-15 15:06:38 -0500

    Laureano (#26)
    * pag games y admin
    
    * laureano
    
    * fix
    
    * laureano
    
    * making responsive

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -4,16 +4,16 @@ import Footer from "../components/Footer";
 import HomeCard from "./HomeCard"
 
 export default function Index() {
-
   return (
     <>
-      <div className="bg-black">
+    
+      <div className="bg-black xsm:hidden">
         <div className="bgimg-1">
           <h2 className="not-italic font-bold text-[55px] leading-[95.19%] text-white text-shadow: 1px 8px 50px rgba(255, 255, 255, 0.25)  mt-52 ">Experience the thrill of Need for Speed</h2>
           <p className="font-normal text-2xl leading-[95.19%] text-white w-[55%]  mt-3">Choose your ride and dominate the streets</p>
           <Button />
         </div>
-        <div className="flex items-center w-full h-[100vh] mt-4">
+        <div className="flex items-center w-full h-[100vh] mt-4 xxsm:hidden">
           <Carousel />
         </div>
         <HomeCard />
